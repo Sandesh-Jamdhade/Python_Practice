b7dbb1bd1652d07325de163838e1e53d20850732
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-01-22 20:23:16 +0530

    Checking appearance of the digits and characters

--- frequency.py
+++ frequency.py
@@ -1,15 +1,21 @@
-#Digit Frequency
-n=123
-count=0
+#Checks how many time the letter appears
+n="sandesh jamdhade"
+freq={}
+for i in n:
+    if i in freq:
+        freq[i]+=1
+    else:
+        freq[i]=1
+print(freq)
+
+#check how many time the digit appears
+n=11111222223333223839399993888
+freq={}
 while n>0:
     i=n%10
-    count+=1
+    if i in freq:
+        freq[i]+=1
+    else:
+        freq[i]=1
     n=n//10
-print(count)
-
-#String Frequency
-n="Sandesh"
-count=0
-for i in n:
-    count+=1
-print(count)
+print(freq)
